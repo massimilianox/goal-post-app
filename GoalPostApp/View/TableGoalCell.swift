403f290fa5600678037a1b761e8329db5bfc9ca7
Mamo <m.abeli@t4project.com>
2018-08-22 21:39:48 +0200

    Goals Table View

---
+++ GoalPostApp/View/TableGoalCell.swift
@@ -0,0 +1,27 @@
+//
+//  TableGoalCell.swift
+//  GoalPostApp
+//
+//  Created by Massimiliano Abeli on 22/08/2018.
+//  Copyright © 2018 Massimiliano Abeli. All rights reserved.
+//
+
+import UIKit
+
+class TableGoalCell: UITableViewCell {
+
+    @IBOutlet weak var goalLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var goalProgressLbl: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    func configureCell(description: String, type: String, progress: Int) {
+        goalLbl.text = description
+        typeLbl.text = type
+        goalProgressLbl.text = String(describing: progress)
+    }
+
+}
